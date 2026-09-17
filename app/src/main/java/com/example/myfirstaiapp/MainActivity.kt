d8fc1892d176a9bf6a65562ca1ec3115c4eff353
carolinaacosta <carolina.acosta@thoughtworks.com>
2026-09-17 12:18:16 +0200

    Create initial compose android app using Android CLI create command

---
+++ app/src/main/java/com/example/myfirstaiapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myfirstaiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.myfirstaiapp.theme.MyFirstAIAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      MyFirstAIAppTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
+    }
+  }
+}
